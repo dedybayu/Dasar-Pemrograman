b338f426bfacd598ab7cbd948efb38f93d1d47dd
dedybayusetiawan <dedybayusetiawan5@gmail.com>
2023-09-28 15:16:31 +0700

    update PemilihanPercobaan107

--- Pertemuan 5 - Pemilihan 1/Sourch Code/PemilihanPercobaan107.java
+++ Pertemuan 5 - Pemilihan 1/Sourch Code/PemilihanPercobaan107.java
@@ -5,9 +5,11 @@ public class PemilihanPercobaan107 {
         System.out.print("Masukan Angka : ");
         int angka = input07.nextInt();
 
+        //Menggunakan Tenary Operator
         String jenisBilangan = (angka % 2 == 0) ? "Bilangan Genap" : "Bilangan Ganjil";
         System.out.println("Angka " + angka + " adalah " + jenisBilangan);
 
+        //Menggunakan if else
         //if (angka % 2 == 0)
         //System.out.println("Angka " + angka + " Bilangan Genap");
         //else

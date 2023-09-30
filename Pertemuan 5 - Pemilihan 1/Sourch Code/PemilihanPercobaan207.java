4a659bdff46950d2115a488d0bb4205a8068a42e
dedybayusetiawan <dedybayusetiawan5@gmail.com>
2023-09-30 19:21:56 +0700

    update

--- Pertemuan 5 - Pemilihan 1/Sourch Code/PemilihanPercobaan207.java
+++ Pertemuan 5 - Pemilihan 1/Sourch Code/PemilihanPercobaan207.java
@@ -14,11 +14,19 @@ public class PemilihanPercobaan207 {
 
       float total = (uas * 0.4F) + (uts * 0.3F) + (kuis * 0.1F) + (tugas * 0.2F);
 
-      String message = (total > 80 && total <= 100) ? "Sangat Baik" : (total > 73 && total <= 80) ? "Lebih dari Baik" :
+      String kualifikasi = (total > 80 && total <= 100) ? "Sangat Baik" : (total > 73 && total <= 80) ? "Lebih dari Baik" :
       (total > 65 && total <= 73) ? "Baik" : (total > 60 && total <= 65) ? "Lebih dari Cukup" : (total > 50 && total <= 60) ? "Cukup" : 
       (total > 39 && total <= 50) ? "Kurang" : "Gagal" ;
+
+      String nilaihuruf = (total > 80 && total <= 100) ? "A" : (total > 73 && total <= 80) ? "B+" :
+      (total > 65 && total <= 73) ? "B" : (total > 60 && total <= 65) ? "C+" : (total > 50 && total <= 60) ? "C" : 
+      (total > 39 && total <= 50) ? "D" : "E" ;
+
+      String nilaisetara = (total > 80 && total <= 100) ? "4" : (total > 73 && total <= 80) ? "3.5" :
+      (total > 65 && total <= 73) ? "3" : (total > 60 && total <= 65) ? "2.5" : (total > 50 && total <= 60) ? "2" : 
+      (total > 39 && total <= 50) ? "1" : "0" ;
       
-      System.out.println("Nilai akhir = " + total + " sehingga nilai anda " + message);
+      System.out.println("Nilai akhir = " + total + ", Nilai Huruf : " + nilaihuruf + ", Setara : " + nilaisetara + ", Kualifikasi : " + kualifikasi);
 
     }
 }

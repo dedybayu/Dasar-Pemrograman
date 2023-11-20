4ed864d870cc6395f38e41dfee871901524e4b7b
dedybayusetiawan <dedybayusetiawan5@gmail.com>
2023-11-20 15:11:54 +0700

    add: modifikasi 3

--- Pertemuan 13 - Fungsi 1/Sourch Code/TerimaKasih_07.java
+++ Pertemuan 13 - Fungsi 1/Sourch Code/TerimaKasih_07.java
@@ -1,6 +1,9 @@
 public class TerimaKasih_07 {
-    public static void main(String[] args) {
+    public static void UcapanTerimaKasih() {
         System.out.println("Thank You for being the best Teacher in the world\n"+
         "You inspired in me a love learning and made me feel like I could ask you anything");
-    }  
+    }
+    public static void main(String[] args) {
+        UcapanTerimaKasih();
+    }
 }
